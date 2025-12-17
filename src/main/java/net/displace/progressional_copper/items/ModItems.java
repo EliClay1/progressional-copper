c2895524cf7eb6fdfc8bd90010f2c3584fe078b8
Eli Clayson <eliclay1.byu.edu>
2025-12-17 12:48:15 -0500

    changed copper text color, added crafting recipe for the template, changed file directory oganization within the data.

--- src/main/java/net/displace/progressional_copper/items/ModItems.java
+++ src/main/java/net/displace/progressional_copper/items/ModItems.java
@@ -6,7 +6,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.SmithingTemplateItem;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -16,6 +15,20 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 public class ModItems {
+
+    private static final Identifier EMPTY_SLOT_HELMET = Identifier.withDefaultNamespace("container/slot/helmet");
+    private static final Identifier EMPTY_SLOT_CHESTPLATE = Identifier.withDefaultNamespace("container/slot/chestplate");
+    private static final Identifier EMPTY_SLOT_LEGGINGS = Identifier.withDefaultNamespace("container/slot/leggings");
+    private static final Identifier EMPTY_SLOT_BOOTS = Identifier.withDefaultNamespace("container/slot/boots");
+    private static final Identifier EMPTY_SLOT_HOE = Identifier.withDefaultNamespace("container/slot/hoe");
+    private static final Identifier EMPTY_SLOT_AXE = Identifier.withDefaultNamespace("container/slot/axe");
+    private static final Identifier EMPTY_SLOT_SWORD = Identifier.withDefaultNamespace("container/slot/sword");
+    private static final Identifier EMPTY_SLOT_SHOVEL = Identifier.withDefaultNamespace("container/slot/shovel");
+    private static final Identifier EMPTY_SLOT_SPEAR = Identifier.withDefaultNamespace("container/slot/spear");
+    private static final Identifier EMPTY_SLOT_PICKAXE = Identifier.withDefaultNamespace("container/slot/pickaxe");
+    private static final Identifier EMPTY_SLOT_NAUTILUS_ARMOR = Identifier.withDefaultNamespace("container/slot/nautilus_armor");;
+
+
     public static DeferredRegister.Items ITEMS = DeferredRegister.createItems(ProgressionalCopper.MOD_ID);
 
     // TODO - the itemID is a new problem with this version, look into this online.
@@ -27,14 +40,18 @@ public class ModItems {
                     Component.translatable("item.progressional_copper.smithing_template.copper_to_iron.ingredients"),
                     Component.translatable("item.progressional_copper.smithing_template.copper_to_iron.base_slot_description"),
                     Component.translatable("item.progressional_copper.smithing_template.copper_to_iron.additions_slot_description"),
-                    List.of(Identifier.withDefaultNamespace("container/slot/ingot")),
-                    // Leaving this blank, may break. Just duplicate above code.
+                    createCopperToIronUpgradeIconList(),
                     List.of(),
                     // Todo - Find a better way to implement the ID. I'm not really sure what is going on here.
                     new Item.Properties().setId(ResourceKey.create(Registries.ITEM,
                             Identifier.fromNamespaceAndPath(ProgressionalCopper.MOD_ID, "copper_to_iron_template")))
             ));
 
+    private static List<Identifier> createCopperToIronUpgradeIconList() {
+        return List.of(EMPTY_SLOT_HELMET, EMPTY_SLOT_SWORD, EMPTY_SLOT_CHESTPLATE, EMPTY_SLOT_PICKAXE,
+                EMPTY_SLOT_LEGGINGS, EMPTY_SLOT_AXE, EMPTY_SLOT_BOOTS, EMPTY_SLOT_HOE, EMPTY_SLOT_SHOVEL,
+                EMPTY_SLOT_NAUTILUS_ARMOR, EMPTY_SLOT_SPEAR);
+    }
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
