2f1e2d21a2ee0159323bb6f05ff966697c3909f3
Eli Clayson <eliclay1.byu.edu>
2025-12-19 16:43:17 -0500

    Added iron pickaxe trade replacement.

--- src/main/java/net/displace/progressional_copper/event/Server.java
+++ src/main/java/net/displace/progressional_copper/event/Server.java
@@ -1,22 +1,33 @@
 package net.displace.progressional_copper.event;
 
 import net.displace.progressional_copper.ProgressionalCopper;
+import net.minecraft.core.Holder;
+import net.minecraft.core.component.DataComponentExactPredicate;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.datafix.fixes.ItemStackCustomNameToOverrideComponentFix;
 import net.minecraft.world.entity.npc.villager.Villager;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentInstance;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
+import net.minecraft.world.item.trading.ItemCost;
 import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraft.world.level.ItemLike;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import net.neoforged.neoforge.event.server.ServerStoppedEvent;
 import net.neoforged.neoforge.event.tick.ServerTickEvent;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @EventBusSubscriber(modid = ProgressionalCopper.MOD_ID)
@@ -46,9 +57,29 @@ public class Server {
             var allOffers = villager.getOffers();
             for (MerchantOffer offer : allOffers) {
                 if (ItemStack.isSameItem(offer.getResult(), Items.IRON_PICKAXE.getDefaultInstance())) {
-                    LOGGER.info("IRON PICK FOUND - Cost A: {}, Cost B: {}, Result {}", offer.getCostA(), offer.getCostB(), offer.getResult());
-                } else {
-                    LOGGER.info("Cost A: {}, Cost B: {}, Result {}", offer.getCostA(), offer.getCostB(), offer.getResult());
+                    LOGGER.info("IRON PICK FOUND - Cost A: {}, Cost B: {}, Result {} \n Changing Offer...",
+                            offer.getCostA(), offer.getCostB(), offer.getResult());
+
+                    List<EnchantmentInstance> enchantmentInstanceList = new ArrayList<>();
+
+                    for (var enchant : offer.getResult().getTagEnchantments().entrySet()) {
+                        Holder<@NotNull Enchantment> enchantment = enchant.getKey();
+                        EnchantmentInstance enchantmentInstance = new EnchantmentInstance(Holder.direct(enchantment.value()),
+                                enchant.getIntValue());
+                        enchantmentInstanceList.add(enchantmentInstance);
+                    }
+                    ItemStack replacementTool = Items.COPPER_PICKAXE.applyEnchantments(Items.COPPER_PICKAXE.getDefaultInstance(),
+                            enchantmentInstanceList);
+
+                    MerchantOffer newOffer = new MerchantOffer(
+                            offer.getItemCostA(),
+                            offer.getItemCostB(),
+                            replacementTool,
+                            offer.getMaxUses(),
+                            offer.getXp(),
+                            offer.getPriceMultiplier()
+                    );
+                    allOffers.set(allOffers.indexOf(offer), newOffer);
                 }
             }
         }
