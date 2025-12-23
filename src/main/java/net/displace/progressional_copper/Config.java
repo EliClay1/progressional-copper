08e7f6e5295b5dc92afaabc9c68d5914b0fa3d4e
Eli Clayson <eliclay1.byu.edu>
2025-12-23 15:23:36 -0500

    updated config todo list

--- src/main/java/net/displace/progressional_copper/Config.java
+++ src/main/java/net/displace/progressional_copper/Config.java
@@ -8,5 +8,10 @@ public class Config {
     public static final ModConfigSpec.BooleanValue REPLACE_DIAMOND_WITH_COPPER_TRADES = BUILDER
             .comment("Removes all diamond trades to match the progression balance")
             .define("replaceDiamondWithCopperTrade", true);
+
+    // TODO - add configuration changes for recipe removal, and maybe even add functionality for lists of items to remove.
+
+    // TODO - configure drop chance from villagers (datagen)
+
     static final ModConfigSpec SPEC = BUILDER.build();
 }
