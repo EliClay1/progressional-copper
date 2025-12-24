63da49b82976384f14bea2769b9cad9c8c02a71a
Eli Clayson <eliclay1.byu.edu>
2025-12-24 13:03:43 -0500

    Implemented smithing template with configuration options.

---
+++ src/main/java/net/displace/progressional_copper/event/LootTableEvents.java
@@ -0,0 +1,89 @@
+package net.displace.progressional_copper.event;
+
+import net.displace.progressional_copper.Config;
+import net.displace.progressional_copper.ProgressionalCopper;
+import net.displace.progressional_copper.items.ModItems;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.LootTableLoadEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.Random;
+
+@EventBusSubscriber(modid = ProgressionalCopper.MOD_ID)
+public class LootTableEvents {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("progressional_copper");
+    private static final String[] smithVillageChests = {"village_toolsmith", "village_weaponsmith"};
+    private static final String[] emeraldVillageChests = {"village_plains_house", "village_desert_house",
+            "village_savanna_house", "village_snowy_house", "village_taiga_house"};
+    private static final Map<Item, Item> copperToIron = Map.ofEntries(
+            Map.entry(Items.COPPER_PICKAXE, Items.IRON_PICKAXE),
+            Map.entry(Items.COPPER_SHOVEL, Items.IRON_SHOVEL),
+            Map.entry(Items.COPPER_SWORD, Items.IRON_SWORD),
+            Map.entry(Items.COPPER_HELMET, Items.IRON_HELMET),
+            Map.entry(Items.COPPER_CHESTPLATE, Items.IRON_CHESTPLATE),
+            Map.entry(Items.COPPER_LEGGINGS, Items.IRON_LEGGINGS),
+            Map.entry(Items.COPPER_BOOTS, Items.IRON_BOOTS),
+            Map.entry(Items.COPPER_HORSE_ARMOR, Items.IRON_HORSE_ARMOR),
+            Map.entry(Items.COPPER_SPEAR, Items.IRON_SPEAR)
+    );
+
+    @SubscribeEvent
+    public static void chestOpen(LootTableLoadEvent event) {
+
+        // emerald increase
+        for (String location : emeraldVillageChests) {
+            int maxCount = 5;
+            int minCount = 1;
+
+            Identifier tableId = event.getName();
+            if (tableId.toString().contains(String.format("village/%s", location))) {
+                LootTable originalTable = event.getTable();
+                float chance = Config.EMERALD_INCREASE_CHANCE.get().floatValue();
+                Random random = new Random();
+                int count = random.nextInt((maxCount - minCount) + 1) + minCount;
+                LootPool customPool = LootPool.lootPool()
+                        .add(LootItem.lootTableItem(Items.EMERALD)
+                                .setWeight(1)
+                                .apply(SetItemCountFunction.setCount(ConstantValue.exactly(count))))
+                        .when(LootItemRandomChanceCondition.randomChance(chance))
+                        .build();
+
+                originalTable.addPool(customPool);
+            }
+        }
+
+        // smithing template
+        for (String location : smithVillageChests) {
+            int minCount = 1;
+
+            Identifier tableId = event.getName();
+            if (tableId.toString().contains(String.format("village/%s", location))) {
+                LootTable originalTable = event.getTable();
+                float chance = Config.SMITHING_TEMPLATE_CHANCE.get().floatValue();
+                Random random = new Random();
+                int count = random.nextInt(1) + minCount;
+                LootPool customPool = LootPool.lootPool()
+                        .add(LootItem.lootTableItem(ModItems.COPPER_TO_IRON_TEMPLATE.get())
+                                .setWeight(1)
+                                .apply(SetItemCountFunction.setCount(ConstantValue.exactly(count))))
+                        .when(LootItemRandomChanceCondition.randomChance(chance))
+                        .build();
+
+                originalTable.addPool(customPool);
+            }
+        }
+    }
+}
